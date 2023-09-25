51b87b9d5f2ba6ecde9b90925bda3531756fe197
Mario Torres <mtorresdesigns@gmail.com>
2023-09-25 16:36:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    scope: {
+      member: '='
+    },
+    controller: function($scope) {
+      $scope.member.skills = $scope.member.skills || [];
+      $scope.addSkill = function() {
+        $scope.member.skills.push({
+          name: '',
+          level: 0
+        });
+      };
+      $scope.removeSkill = function(index) {
+        $scope.member.skills.splice(index, 1);
+      };
+    }
+  };
+}
